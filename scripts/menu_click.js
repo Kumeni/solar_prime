490d54086173ee9e474e51b677c6e715adfaac95
Kumeni <samsonkumeni8@gmail.com>
2023-05-12 23:55:03 +0300

    homepage responsive design

---
+++ scripts/menu_click.js
@@ -0,0 +1,43 @@
+let menuState = "CLOSED", lastScrollPos;
+const handleMenuClick = () => {
+    let bars = document.getElementById("menu_bars").getElementsByTagName("div"), i,
+    main = document.getElementsByTagName("main")[0],
+    footer = document.getElementsByTagName("footer")[0],
+    menu_info = document.getElementsByClassName("menu_info")[0],
+    header = document.getElementsByTagName("header"), menuInfoHeight;
+
+    if(menuState === "CLOSED"){
+        for(i=1; i<bars.length; i++){
+            bars[i].style.width = "100%";
+        }
+        
+        /*lastScrollPos = window.scrollY;
+        window.scrollTo(0,0);
+        menuInfoHeight = menu_info.getBoundingClientRect().height;
+
+        main.style.height = menuInfoHeight + "px";
+        menu_info.style.marginLeft = "0px";
+        footer.style.display = "none";*/
+
+        menuState = "OPEN";
+    
+    } else if( menuState === "OPEN"){
+        for (i=1; i<bars.length; i++){
+            if(i === 1)
+                bars[i].style.width = "50%";
+            else
+                bars[i].style.width = "25%";
+        }
+
+        /*main.style.height = "auto";
+        menu_info.style.marginLeft = -window.innerWidth +"px";
+        footer.style.display = "block";
+        if(lastScrollPos !== undefined)
+            window.scrollTo(0, lastScrollPos);*/
+        menuState = "CLOSED";
+    }
+    
+    return null;
+
+}
+
